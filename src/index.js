8785061ac359df681e5d573e19e74ec1e874fabf
Shukla <priyashukla@deloitte.com>
2021-12-28 17:02:10 +0530

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
+import './components/bootstrap.min.css'
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
